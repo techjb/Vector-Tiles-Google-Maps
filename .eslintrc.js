20667a1333c0faf14c59a9809d54471ba9a6a4f9
Seth Williams <92401567+swilliams-northpointkc@users.noreply.github.com>
2023-01-24 11:56:24 -0600

    BMAPS-1689 Fork Vector-Tiles-Google-Maps and set up as NPM package (#1)
    * Clean up VSCode-related files
    
    * Add npm project and configs
    
    * Add config files
    
    * Add basic unit test
    
    * Add sourceType of module to eslint configs
    
    * Restore example screenshot

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: {
+    'jest': true,
+    'browser': true,
+    'node': true,
+  },
+  extends: [
+    'eslint:recommended',
+    'google',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
+  ],
+  parserOptions: {
+    'ecmaVersion': 2022,
+    'sourceType': 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'require-jsdoc': 'off',
+    'max-len': ['error', {
+      'ignoreStrings': true,
+      'ignoreTemplateLiterals': true,
+      'ignoreRegExpLiterals': true,
+      'ignoreUrls': true,
+      'code': 120,
+    }],
+    'jest/no-disabled-tests': ['error'],
+  },
+};
---
+++ tests/helloWorld.spec.js
@@ -0,0 +1,5 @@
+describe('Making sure Jest works', () => {
+  it('works', () => {
+    expect(undefined).toBeUndefined();
+  });
+});
